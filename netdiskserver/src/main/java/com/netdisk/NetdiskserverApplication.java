32b680b07922206daac0737975da36acf7276da1
zihui <3335914015@qq.com>
2023-06-24 15:50:11 +0800

    create project

---
+++ netdiskserver/src/main/java/com/netdisk/NetdiskserverApplication.java
@@ -0,0 +1,14 @@
+package com.netdisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetdiskserverApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NetdiskserverApplication.class, args);
+	}
+
+}
---
+++ netdiskserver/src/test/java/com/netdisk/NetdiskserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetdiskserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
