7e612ebbaee64469d19fb979d7d6f7d870d7443a
zihui <3335914015@qq.com>
2023-06-25 15:44:18 +0800

    后端构建 小demo /api/test  return hello

--- netdiskserver/src/main/java/com/netdisk/NetdiskserverApplication.java
+++ netdiskserver/src/main/java/com/netdisk/NetdiskserverApplication.java
@@ -2,8 +2,14 @@ package com.netdisk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@EnableAsync
+@SpringBootApplication(scanBasePackages = {"com.netdisk"})
+@EnableTransactionManagement
+@EnableScheduling
 public class NetdiskserverApplication {
 
 	public static void main(String[] args) {
